c263405368a4d1d7bb74caa3ee7424092c5f320a
victorivanov <prgmac@mail.ru>
2025-05-16 20:25:22 +0200

    try 1

--- main.go
+++ main.go
@@ -10,6 +10,6 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Привет, мир!")
 	})
-	log.Println("Сервер запущен на :8074")
-	log.Fatal(http.ListenAndServe(":8074", nil))
+	log.Println("Сервер запущен на :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
