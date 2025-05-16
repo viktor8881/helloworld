31519b177eb2e1acaf6d1f66be8d683834b2b0c8
victorivanov <prgmac@mail.ru>
2025-05-16 21:33:16 +0200

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Привет, мир!")
+		fmt.Fprintln(w, "Привет, весь мир!")
+		fmt.Fprintln(w, "Привет, весь мир!")
 	})
 	log.Println("Сервер запущен на :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
