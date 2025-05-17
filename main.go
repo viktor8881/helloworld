a34631fd245a9adf211c65184c7ba2c40d78d2ce
victorivanov <prgmac@mail.ru>
2025-05-17 15:14:46 +0200

    Add greeting message for "добра утра" in main.go

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 		fmt.Fprintln(w, "Привет, весь мир!")
 		fmt.Fprintln(w, "Привет, весь мир!")
 		fmt.Fprintln(w, "Работает! я спать.")
+		fmt.Fprintln(w, "Работает! добра утра.")
 	})
 	log.Println("Сервер запущен на :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
